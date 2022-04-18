2cf154e2cde77a5bb877bd2d1f5b9c8a96885a65
ianchen23 <ianchen23@gmail.com>
2022-04-17 23:11:34 -0500

    Math facts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useCallback, useState } from 'react';
+import { Box, Button, Paper } from '@mui/material';
+import Fact from './Fact';
 
 function App() {
+  const total = 20;
+  const [res, setRes] = useState(0);
+  const [allowEdits, setAllowEdits] = useState(false);
+  const updateResult = useCallback((update) => {
+    setRes(res + update);
+    console.log(res);
+  }, [res]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper elevation={3} sx={{ m: 1 }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+          {[...Array(total).keys()].map(_i => <Fact updateResult={updateResult} allowEdits={allowEdits} />)}
+        </Box>
+        <Box sx={{ fontSize: '2rem' }}>{res} / {total}</Box>
+        <Button variant="contained" color='warning' onClick={() => setAllowEdits(true)} sx={{ m: 2 }}>Try Again</Button>
+        <Button variant="contained" onClick={() => window.location.reload()} sx={{ m: 2 }}>Try More</Button>
+      </Paper>
     </div>
   );
 }
